3c0388d36b48f309b1de6d3749ae27c38311c23f
correialeo <leandroo774@outlook.com>
2025-04-28 11:42:05 -0300

    feat: conexao com oracle feita, migracao e update realizados

--- Trackin.API/Program.cs
+++ Trackin.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Trackin.API.Infrastructure.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TrackinContext>(options =>
+{
+    options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
+}
+            );
 
 var app = builder.Build();
 
