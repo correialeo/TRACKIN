da8abd09a0c258fd908c3daef1a91aef13da1157
correialeo <leandroo774@outlook.com>
2025-04-28 11:41:46 -0300

    feat: mapeamento e contexto feitos

---
+++ Trackin.API/Infrastructure/Context/TrackinContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trackin.API.Domain.Entity;
+
+namespace Trackin.API.Infrastructure.Context
+{
+    public class TrackinContext(DbContextOptions<TrackinContext> options) : DbContext(options)
+    {
+        public DbSet<LocalizacaoMoto> Localizacoes { get; set; }
+        public DbSet<Moto> Motos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Patio> Patios { get; set; }
+        public DbSet<Filial> Filiais { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TrackinContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Trackin.API/Infrastructure/Mappings/FilialMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Trackin.API.Domain.Entity;
+
+namespace Trackin.API.Infrastructure.Mappings
+{
+    public class FilialMapping : IEntityTypeConfiguration<Filial>
+    {
+        public void Configure(EntityTypeBuilder<Filial> builder)
+        {
+            builder.HasKey(f => f.Id);
+            builder.Property(f => f.Id).ValueGeneratedOnAdd();
+
+            builder.Property(f => f.Nome).IsRequired().HasMaxLength(100);
+            builder.Property(f => f.Telefone).IsRequired().HasMaxLength(20);
+            builder.Property(f => f.Email).IsRequired().HasMaxLength(100);
+
+            builder.HasOne(f => f.Patio) 
+                   .WithOne(p => p.Filial)
+                   .HasForeignKey<Filial>(f => f.PatioId);
+
+        }
+    }
+}
