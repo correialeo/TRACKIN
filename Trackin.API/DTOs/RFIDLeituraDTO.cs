ffe8d8d2e798ec590296a173128d2c3263201e33
correialeo <leandroo774@outlook.com>
2025-04-29 12:13:06 -0300

    feat: dtos para localizacao e leitura do rfid implementados

---
+++ Trackin.API/DTOs/RFIDLeituraDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Trackin.API.DTOs
+{
+    public class RFIDLeituraDTO
+    {
+        public string RFID { get; set; } 
+        public long SensorId { get; set; }
+        public double CoordenadaX { get; set; }
+        public double CoordenadaY { get; set; }
+    }
+}
