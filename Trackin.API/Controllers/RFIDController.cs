0fdae11aa02e159834f3c88e39b2b94fdf5795b2
correialeo <leandroo774@outlook.com>
2025-04-29 12:13:41 -0300

    feat: rfidController implementado

---
+++ Trackin.API/Controllers/RFIDController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Trackin.API.DTOs;
+using Trackin.API.Services;
+
+namespace Trackin.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RFIDController : ControllerBase
+    {
+        private readonly RFIDService _service;
+        public RFIDController(RFIDService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProcessarLeituraRFID([FromBody] RFIDLeituraDTO leitura)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var response = await _service.ProcessarLeituraRFID(leitura);
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+            return Ok(response.Data);
+        }
+
+    }
+}
